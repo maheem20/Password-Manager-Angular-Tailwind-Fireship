1e3fb696ab79757b14c372115b8836f3bebf766c
Maheem <88165826+maheem20@users.noreply.github.com>
2023-09-10 21:23:34 +0000

    create login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SiteListComponent } from './site-list/site-list.component';
 import { PasswordListComponent } from './password-list/password-list.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     SiteListComponent,
-    PasswordListComponent
+    PasswordListComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
