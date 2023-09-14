11ee0d8e9ff85091ad1623be8ac62ff16c42409c
Maheem <88165826+maheem20@users.noreply.github.com>
2023-09-14 20:58:17 +0000

    define routes for application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SiteListComponent } from './site-list/site-list.component';
+import { PasswordListComponent } from './password-list/password-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component : LoginComponent },
+  { path: 'site-list', component : SiteListComponent },
+  { path: 'password-list', component : PasswordListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
