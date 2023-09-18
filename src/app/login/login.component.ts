cb843c243fd2c26b59d2fc4cfcf1477c6e2673b5
maheem_20 <mwistanbuly@gmail.com>
2023-09-18 23:20:32 +0200

    inject Router into constructor

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PasswordManagerService } from '../password-manager.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,7 @@ import { PasswordManagerService } from '../password-manager.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  constructor(private passwordManagerService: PasswordManagerService) { }
+  constructor(private passwordManagerService: PasswordManagerService, private router: Router) { }
 
   onSubmit(values: any) {
     this.passwordManagerService.login(values.email, values.password).then(() => {
