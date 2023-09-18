3392b479f03981355ba23c06cc27dd258c01a379
maheem_20 <mwistanbuly@gmail.com>
2023-09-18 23:17:15 +0200

    define onSubmit method which is called when form is submitted

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,4 +8,12 @@ import { PasswordManagerService } from '../password-manager.service';
 })
 export class LoginComponent {
   constructor(private passwordManagerService: PasswordManagerService) { }
+
+  onSubmit(values: any) {
+    this.passwordManagerService.login(values.email, values.password).then(() => {
+      console.log('Login successful');
+    }).catch(err => {
+      console.log(err);
+    });
+  }
 }
