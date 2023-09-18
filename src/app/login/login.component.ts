6bfad9dbe97cc94ba4b1732817f2e891b4de4458
maheem_20 <mwistanbuly@gmail.com>
2023-09-18 23:15:41 +0200

    inject PasswordManagerService into constructor

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PasswordManagerService } from '../password-manager.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  constructor(private passwordManagerService: PasswordManagerService) { }
 }
